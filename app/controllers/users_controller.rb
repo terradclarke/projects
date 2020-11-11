cc85691e5ed12f2e23d25e7a80c9ec2e90aa4809
hugodelgado <hugo.delgado1@outlook.com>
2020-11-11 08:13:25 -0600

    codal

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    before_action :must_be_admin
+
+    def index
+        @users = User.all
+    end
+
+    private
+    def must_be_admin
+        unless current_user.admin?
+            redirect_to appointments_path, alert: "You don't have access to this page"
+        end
+    end
+end
